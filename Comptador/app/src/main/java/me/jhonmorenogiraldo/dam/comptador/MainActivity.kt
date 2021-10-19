c1f6397a307e60f0c2d6042c326abde68d29b474
Jhon <jmoreno1@iesebre.com>
2021-10-19 16:28:21 +0200

    v.0

--- Comptador/app/src/main/java/me/jhonmorenogiraldo/dam/comptador/MainActivity.kt
+++ Comptador/app/src/main/java/me/jhonmorenogiraldo/dam/comptador/MainActivity.kt
@@ -2,15 +2,43 @@ package me.jhonmorenogiraldo.dam.comptador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-
-
-
+    internal lateinit var tapMeButton : Button
+    internal lateinit var timeTextView : TextView
+    internal lateinit var counterTextView : TextView
+    internal var counter = 0
+    internal var time = 60
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main2)
+
+        tapMeButton = findViewById(R.id.tapMeButton)
+        timeTextView = findViewById(R.id.timeTextView)
+        counterTextView = findViewById(R.id.counterTextView)
+
+        //Actualitzar o definir valor inicial del countertextview
+
+        //TODO en algun moment hauren d'executar incrementCounter
+
+        tapMeButton.setOnClickListener {
+            incrementCounter()
+            //TODO Iniciar el comptador
+        }
+//        timeTextView.text = time.toString()
+       timeTextView.text = getString(R.string.timeText, time)
+
     }
+
+   private fun incrementCounter(){
+       //       counter = counter +1
+       counter += 1
+       counterTextView.text =counter.toString()
+
+   }
 }
